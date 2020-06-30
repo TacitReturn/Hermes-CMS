94121bdaab825893cfbc95d51cc79b10f0c6477e
TacitReturn <glennrudge@outlook.com>
2020-06-29 20:26:49 -0400

    made changes to file directory

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <h1 id="header">Hermes MVC</h1>
+<img src="hermese-logo.png" width="100px" alt="">
 <p>A simple minimal and generic PHP MVC framework.</p>
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,11 @@
 <?php
+/**
+ *
+ * This file loads all of the needed libraries for the Hermes
+ * Files loaded: Core.php, controller.php, Database.php
+ * You may configure this file to load additional files if need be.
+ */
+
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
